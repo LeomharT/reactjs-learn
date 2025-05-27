cd9334219c2a297f04950c8a3dca7821b62733c1
LeomharT <leo.liao.lzy@gmail.com>
2025-05-27 10:53:00 +0800

    update:learning useTransition hook

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,12 +6,14 @@ import {
 } from 'react-router';
 import App from '../app/App';
 import DemoUseOptimistic from '../pages/DemoUseOptimistic';
+import DemoUseTransition from '../pages/DemoUseTransition';
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
 			<Route index element={<Navigate to='/useOptimistic' />} />
 			<Route path='useOptimistic' element={<DemoUseOptimistic />} />
+			<Route path='useTransition' element={<DemoUseTransition />} />
 		</Route>
 	)
 );
