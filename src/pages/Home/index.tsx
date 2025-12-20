3695a804bf434cfc62ae3c220ccf878cf86d0765
LeomharT <leo.liao.lzy@gmail.com>
2025-12-20 23:21:54 +0800

    feat: vmap

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,29 @@
+import { Card, Flex } from 'antd';
+import jsVectorMap from 'jsvectormap';
+import 'jsvectormap/dist/jsvectormap.min.css';
+import 'jsvectormap/dist/maps/world.js';
+
+import { useEffect, useRef } from 'react';
 export default function Home() {
-  return <div>Home</div>;
+  const map = useRef<typeof jsVectorMap>(null);
+
+  useEffect(() => {
+    const el = document.querySelector('#map');
+    if (el) el.innerHTML = '';
+
+    map.current = new jsVectorMap({
+      selector: '#map',
+      map: 'world',
+      toolbar: false,
+      height: 500,
+    });
+  }, []);
+
+  return (
+    <Flex vertical>
+      <Card>
+        <div id='map'></div>
+      </Card>
+    </Flex>
+  );
 }
